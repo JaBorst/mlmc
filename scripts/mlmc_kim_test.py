00f09d561ccb79af2a4141d3fae4b77ff7d93c19
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2019-12-10 10:50:10 +0100

    latest experiment

--- scripts/mlmc_kim_test.py
+++ scripts/mlmc_kim_test.py
@@ -7,7 +7,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 import mlmc
 
 
-weights, vocabulary = mlmc.helpers.load_glove(embedding="/disk1/users/jborst/Data/Notebooks/course_material/data/glove/glove.6B.50d.txt")
+weights, vocabulary = mlmc.helpers.load_glove(embedding="/disk1/users/jborst/Data/Embeddings/glove/en/glove.6B.50d.txt")
 
 data = mlmc.data.get_dataset_only("appd", ensure_valid=True, valid_split=0.25)
 tc = mlmc.models.LabelSpecificAttention(data["classes"], weights, vocabulary,
@@ -17,5 +17,5 @@ tc = mlmc.models.LabelSpecificAttention(data["classes"], weights, vocabulary,
                                         device=device)
 
 
-_ = tc.fit(data["train"], data["valid"], epochs=45, batch_size=50)
+_ = tc.fit(data["train"], data["valid"], epochs=100, batch_size=50)
 tc.evaluate(data["train"])
