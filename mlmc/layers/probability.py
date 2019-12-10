f9708174835b819ff33ad6d66ff91c68baae0090
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2019-12-10 10:38:52 +0100

    Proability propagation layers

---
+++ mlmc/layers/probability.py
@@ -0,0 +1,12 @@
+import torch
+
+class Prob(torch.nn.Module):
+    def __init__(self, n_labels):
+        super(Prob,         self).__init__()
+        self.corr = torch.nn.Parameter(torch.Tensor(n_labels, n_labels))
+        torch.nn.init.eye_(self.corr)
+
+    def forward(self, x):
+        x = (torch.unsqueeze(x, -1) * self.corr).sum(-2)
+        return x
+
