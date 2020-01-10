c4b9d037a5bd9cad91e7aa3357c4c54d63c7f598
Unknown <mai13ivw@studserv.uni-leipzig.de>
2020-01-10 18:48:16 +0100

    changed defualt argument in embedding loader

--- mlmc/helpers/embeddings/embedding_loaders.py
+++ mlmc/helpers/embeddings/embedding_loaders.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def load_glove(embedding="/disk1/users/jborst/Data/Notebooks/course_material/data/glove/glove.6B.50d.txt"):
+def load_glove(embedding="/disk1/users/jborst/Data/Embeddings/glove/en/glove.6B.50d_small.txt"):
     glove = np.loadtxt(embedding, dtype='str', comments=None)
     glove = glove[np.unique(glove[:,:1],axis=0, return_index=True)[1]]
     words = glove[:, 0]
