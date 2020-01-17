77f3cbd37c2a44001453006e2bde3dfaaadef7b4
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2020-01-17 10:32:41 +0100

    package stuff

--- mlmc/data/__init__.py
+++ mlmc/data/__init__.py
@@ -75,6 +75,13 @@ def get_dataset(name, type, ensure_valid=False, valid_split=0.25, target_dtype=t
 
         return datasets
 
+def sample(dataset, fraction=None, absolute=None):
+    from numpy.random import choice
+    n_samples = absolute if absolute is not None else  int(fraction*len(dataset))
+    ind = choice(range(len(dataset)), n_samples)
+    x = [dataset.x[i] for i in ind]
+    y = [dataset.y[i] for i in ind]
+    return type(dataset)(x=x, y=y, classes=dataset.classes)
 
 # d = get_dataset("conll2003en", type=mlmc.data.SequenceDataset, target_dtype=torch._cast_Long)
 # for b in torch.utils.data.DataLoader(d["train"], batch_size=15): break
--- mlmc/models/__init__.py
+++ mlmc/models/__init__.py
@@ -1,1 +1,3 @@
 from .multilabel_models import KimCNN, KimCNNProb, LabelSpecificAttention, XMLCNN, LabelScoringModel, LabelAttentionScoringModel
+from .zeroshot_models import LabelScoringGraphModel
+from .LSAN import LSANOriginal
