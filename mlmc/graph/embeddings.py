462d52c23fe60a62955c98382e1a656ca226b3d0
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2020-01-24 14:12:57 +0100

    Signatures of Graph embedding functions
    Changed the graph embedding functions to graph input to make it possible to retrieve embeddings of subgraphs after calculating on the complete graph structure.

--- mlmc/graph/embeddings.py
+++ mlmc/graph/embeddings.py
@@ -3,22 +3,36 @@
 from sklearn.decomposition import NMF
 from sklearn import random_projection
 from node2vec import Node2Vec
+import numpy as np
 
 import networkx as nx
+def subgraph_extract(X, graph, subnodelist):
+    new = np.zeros_like(X)
+    for i, nm in enumerate(graph.nodes):
+        if nm in subnodelist.keys():
+            new[subnodelist[nm]] = X[i]
+    return new
 
-def get_node2vec(X, dim):
-    graph = nx.from_numpy_matrix(X)
+
+def get_node2vec(graph, classes, dim, return_all=False):
     node2vec = Node2Vec(graph, dimensions=dim, walk_length=30, num_walks=500, workers=4)
     model = node2vec.fit(window=50, min_count=1, batch_words=4)
-    return model.wv.vectors
-
-def get_nmf(X, dim):
-    model = NMF(n_components=dim, init='random', random_state=0)
-    W = model.fit_transform(X)
-    H = model.components_
+    W = model.wv.vectors
+    if return_all: W=subgraph_extract(W,graph, dict(zip(graph.nodes, len(range(graph.nodes)))))
+    else: W=subgraph_extract(W,graph,classes)
     return W
 
-def get_random_projection(X, dim):
+def get_nmf(graph, classes, dim, return_all=False):
+    model = NMF(n_components=dim, init='random', random_state=0)
+    W = model.fit_transform(nx.to_numpy_array(graph))
+    H = model.components_
+    if return_all: W=subgraph_extract(W,graph, dict(zip(graph.nodes, len(range(graph.nodes)))))
+    else: W=subgraph_extract(W,graph,classes)
+    return W
+
+def get_random_projection(graph, classes, dim, return_all=False):
     transformer = random_projection.GaussianRandomProjection(n_components=dim)
-    W = transformer.fit_transform(X)
+    W = transformer.fit_transform(nx.to_numpy_array(graph))
+    if return_all: W=subgraph_extract(W,graph, dict(zip(graph.nodes, len(range(graph.nodes)))))
+    else: W=subgraph_extract(W,graph,classes)
     return W
