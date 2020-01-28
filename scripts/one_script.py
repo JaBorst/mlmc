dd5848f4c3b650e2f8015297eb1a577d1363938f
Unknown <mai13ivw@studserv.uni-leipzig.de>
2020-01-28 19:27:49 +0100

    onescript update

--- scripts/one_script.py
+++ scripts/one_script.py
@@ -45,7 +45,7 @@ class Sequence2SequenceAbstract(torch.nn.Module):
                             dtype=length.dtype).expand(len(length), max_len) < length.unsqueeze(1)
         if dtype is not None:
             mask = torch.as_tensor(mask, dtype=dtype, device=length.device)
-        return mask
+        return mask.to(self.device)
 
     def predict(self, sentence):
         self.eval()
@@ -121,7 +121,7 @@ class Sequence2SequenceAbstract(torch.nn.Module):
 
 import string
 class BILSTMCNN(Sequence2SequenceAbstract):
-    def __init__(self, vocabulary, weights,  classes, word_length=45, alphabet=string.ascii_letters+"0123456789",
+    def __init__(self, vocabulary, weights,  classes, word_length=45, alphabet=string.ascii_letters+string.punctuation+"0123456789",
                  dropout: float = 0.0,
                  word_dropout: float = 0.05,
                  locked_dropout: float = 0.5
@@ -133,12 +133,12 @@ class BILSTMCNN(Sequence2SequenceAbstract):
         self.vocabulary = vocabulary
         self.word_length=word_length
         self.alphabet = alphabet
-        self.filters=53
+        self.filters=50
         self.use_dropout: float = dropout
         self.use_word_dropout: float = word_dropout
         self.use_locked_dropout: float = locked_dropout
         self.sorted_batches=False
-        self.kernel_sizes=[3]
+        self.kernel_sizes=[3,4]
         self.c_embedding_dim = 30
         self.lstm_hidden = 275
 
@@ -155,7 +155,8 @@ class BILSTMCNN(Sequence2SequenceAbstract):
         self.w_embedding.from_pretrained(torch.FloatTensor(weights), freeze=True)
 
         self.c_embedding = torch.nn.Embedding(len(self.alphabet)+2, self.c_embedding_dim)
-        self.convs = torch.nn.ModuleList([torch.nn.Conv1d(self.c_embedding_dim, self.filters, k) for k in self.kernel_sizes])
+        # self.convs = torch.nn.ModuleList([torch.nn.Conv1d(self.c_embedding_dim, self.filters, k) for k in self.kernel_sizes])
+        self.char_lstm=torch.nn.LSTM(input_size=self.c_embedding_dim,hidden_size=50,batch_first=True,bidirectional=True)
 
         h0 = torch.zeros(2, 1, self.lstm_hidden)
         c0 = torch.zeros(2, 1, self.lstm_hidden)
@@ -163,7 +164,7 @@ class BILSTMCNN(Sequence2SequenceAbstract):
         torch.nn.init.xavier_normal_(c0, gain=torch.nn.init.calculate_gain('relu'))
         self.h0 = torch.nn.Parameter(h0, requires_grad=True)  # Parameter() to update weights
         self.c0 = torch.nn.Parameter(c0, requires_grad=True)
-        self.lstm = mlmc.layers.LSTM(weights.shape[1]+len(self.convs)*self.filters, self.lstm_hidden, dropout=0.5,
+        self.lstm = mlmc.layers.LSTM(weights.shape[1]+len(self.kernel_sizes)*self.filters, self.lstm_hidden, dropout=0.0,
                                        bidirectional=True, batch_first=True)
         self.projection = torch.nn.Linear(in_features=2*self.lstm_hidden, out_features=self.n_classes, )
         self.crf = CRF(len(self.classes), batch_first=True)
@@ -176,20 +177,27 @@ class BILSTMCNN(Sequence2SequenceAbstract):
                  for conv in self.convs],
                 dim=-1)
 
+    def do_char_lstm(self,x):
+        output = []
+        for i in range(x.shape[1]):
+            o = self.char_lstm(x[:, i, :, :])[1][0]
+            output.append(torch.cat([o[0],o[1]],dim=-1))
+        return torch.stack(output,dim=1)
+
     def forward(self, x, truth=None):
-        mask = self.get_mask(x[0]).squeeze()
+        mask = self.length_to_mask(x[2],max_len=x[0].shape[1]).squeeze()
 
         #character embeddings
         c_embed = self.c_embedding(x[1])
         if self.use_dropout > 0.0: c_embed = self.dropout(c_embed)
-        char_embeddings = self.do_char_convs(c_embed.transpose(-1,-2))
-
+        # char_embeddings = self.do_char_convs(c_embed.transpose(-1,-2))
+        char_embeddings = self.do_char_lstm(c_embed)
         #Word Embeddings
         w_embed = self.w_embedding(x[0])
 
         #Combined Embeddings
         embedding = torch.cat([w_embed, char_embeddings], dim=-1)
-        # if self.use_dropout > 0.0: embedding = self.dropout(embedding)
+        if self.use_dropout > 0.0: embedding = self.dropout(embedding)
 
         packed_embedding = torch.nn.utils.rnn.pack_padded_sequence(embedding, x[2], batch_first=True, enforce_sorted=self.sorted_batches)
         r, _ = self.lstm(packed_embedding, (self.h0.repeat(1, x[0].shape[0], 1),self.c0.repeat(1, x[0].shape[0], 1)))
@@ -233,10 +241,11 @@ class BILSTMCNN(Sequence2SequenceAbstract):
             return (words, chars, length)
 
 
-
+data["classes"] = dict(zip(data["classes"], range(1,len(data["classes"])+1)))
+data["classes"]["0"]=0
 blc = BILSTMCNN(vocabulary, weights, data["classes"],optimizer=torch.optim.SGD,
                             optimizer_params={"lr": 0.01}, device=device,
-                            locked_dropout=0.0 ,word_dropout=0.01, dropout=0.65
+                            locked_dropout=0.0 ,word_dropout=0.00, dropout=0.65
                             )
 
 blc.fit(data["train"], data["valid"], epochs=100, batch_size=10)
