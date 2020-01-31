f08b61334285fc12fda5a2f773d7083aa6f942f0
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2020-01-31 13:37:57 +0100

    Make geometric optional

--- mlmc/layers/__init__.py
+++ mlmc/layers/__init__.py
@@ -1,4 +1,7 @@
 from .probability import Prob
 from .lstm import LSTM, LSTMRD
 from .label_layers import LabelEmbeddingScoring,AdaptiveCombination,LabelAttention, LabelSpecificSelfAttention
-from .graph import GatedGraphConv
+try:
+    from .graph import GatedGraphConv
+except:
+    print("pytorch_geometric not installed. Install if you want to use Geometric models")
