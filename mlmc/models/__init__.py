33d9b44a248174c890656d55560a7e3eff2e1a7d
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2020-01-31 13:39:33 +0100

    geometric models

--- mlmc/models/__init__.py
+++ mlmc/models/__init__.py
@@ -1,8 +1,6 @@
 from .XMLCNN import XMLCNN
-from .zeroshot_models import LabelScoringGraphModel
 from .LSAN import LSANOriginal
-from .ZAGCNN import ZAGCNN
-from .SKG_ML import SKG
+
 from .LSAN_reimplementation import LabelSpecificAttention
 from .LSANOriginalTransformer import LSANOriginalTransformer
 from .ConceptScores import ConceptScores, ConceptScoresCNN,ConceptScoresCNNAttention,KimCNN2Branch,ConceptProjection,ConceptScoresAttention, ConceptScoresRelevance
@@ -11,6 +9,13 @@ from .KimCNN import KimCNN
 
 from .save_and_load import load,save
 
+#geomtric models
 
+try:
+    from .zeroshot_models import LabelScoringGraphModel
+    from .ZAGCNN import ZAGCNN
+    from .SKG_ML import SKG
+except:
+    pass
 
 
