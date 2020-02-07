58ff15ce03a1930960ecb8ec1ebd3e6802ec20e3
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2020-02-07 15:41:09 +0100

    cleaned GloveConcept Model

--- mlmc/models/GloveAsConcept.py
+++ mlmc/models/GloveAsConcept.py
@@ -6,7 +6,6 @@ from .abstracts import TextClassificationAbstract
 from ..representation import get, is_transformer
 import re
 from ..representation.labels import makemultilabels
-import torch_geometric as torch_g
 from ..layers import Bilinear
 
 class GloveConcepts(TextClassificationAbstract):
@@ -50,50 +49,20 @@ class GloveConcepts(TextClassificationAbstract):
         self.labels.requires_grad = False
 
 
-        # self.query_projection = torch.nn.Linear(self.embedding_dim,200)
-        # self.key_projection = torch.nn.Linear(self.embedding_dim,200)
 
         self.input_projection = torch.nn.Linear(self.embedding_dim, 512)
         self.input_projection2 = torch.nn.Linear(self.concept_embedding_dim, self.embedding_dim)
         self.second_projection = torch.nn.Linear(512, self.compare_space_dim)
         self.second_projection2 = torch.nn.Linear(512, self.compare_space_dim)
-        # self.concept_projection = torch.nn.Linear(2048, self.n_classes)
-        # self.output_projection = torch.nn.Linear(300, self.n_classes,bias=False)
-        # self.output_projection.weight.requires_grad =False
-        # with torch.no_grad():
-        #     self.output_projection.weight.copy_(self.concept_aggregation_labels)
-
-        # self.importance = torch.nn.Linear(self.embedding_dim,1).to(self.device)
-        #
-        # self.query_projection = torch.nn.Linear(self.embedding_dim, self.att_dim)
-        # self.key_projection = torch.nn.Linear(self.embedding_dim, self.att_dim)
-        # self.scaling = torch.nn.Parameter(torch.sqrt(torch.Tensor([1./400.])))
-
-        # self.comparing_space_doc = torch.nn.Linear(self.n_concepts, 256)
-        # self.comparing_space_label = torch.nn.Linear(self.n_concepts, 256)
-
-        # self.ggc = torch_g.nn.GraphConv(self.n_concepts, self.n_concepts).to(self.device)
-        # self.metric_tensor = torch.nn.Parameter(
-        #     torch.triu(torch.rand((self.concept_embedding_dim, self.concept_embedding_dim)))#+  self.concept_embedding_dim*torch.eye(self.concept_embedding_dim)
-        # )
-        # self.metric_tensor.requires_grad=True
-        # torch.nn.init.kaiming_normal_(self.metric_tensor)
         self.metric = Bilinear(self.embedding_dim)
         self.output_projection = torch.nn.Linear(in_features=self.max_len*self.n_classes, out_features=self.n_classes)
-        # self.output_projection = torch.nn.Linear(in_features=self.max_len*512, out_features=self.n_classes)
         self.build()
 
     def forward(self, x, return_scores=False):
         with torch.no_grad():
             embeddings = torch.cat(self.embedding(x)[2][(-1 - self.n_layers):-1], -1)
-            # embeddings = embeddings.flatten(1,2)
-        #
-        # p = self.input_projection(embeddings)
         p2= self.input_projection2(self.labels)
         return self.output_projection(self.metric(embeddings,p2).flatten(1,2))
-        # return self.output_projection(p.flatten(1,2))
 
     # def regularize(self):
-    #     lower_tri = torch.triu(self.metric_tensor).t()
-    #     dev = torch.relu((lower_tri.sum(-1) - 2 * lower_tri.diag()))
-    #     return  dev.sum() + torch.relu(-lower_tri.diag()).sum()
+    #     return self.metric.regularize()
