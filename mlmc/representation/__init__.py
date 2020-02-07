dadf56581cb3c0c66e5478b3c0f0136a8e88d128
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2020-02-07 19:14:08 +0100

    Added Embedding postprocessing funtion
    https://arxiv.org/pdf/1702.01417.pdf

--- mlmc/representation/__init__.py
+++ mlmc/representation/__init__.py
@@ -1,3 +1,4 @@
 from .representations import load_static, get_transformer, map_vocab,get_embedding,get, is_transformer
 from .character import charindex
 from .labels import makesequencelabels, schemetransformer, to_scheme, makemultilabels
+from .postprocessing_vectors import postprocess_embedding
