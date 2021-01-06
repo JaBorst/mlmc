561a46e81f4a897a49db669494067f7a7e2593dc
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2021-01-06 10:54:40 +0100

    Install torch_geometric from within

--- mlmc/__init__.py
+++ mlmc/__init__.py
@@ -20,3 +20,22 @@ from .save_and_load import save, load
 
 import logging
 logging.getLogger("transformers.tokenization_utils_base").setLevel(logging.ERROR)
+
+import subprocess
+import sys
+import torch
+
+
+def install_torch_geometric():
+    cuda = f"cu{torch.version.cuda.replace('.','')}"
+    pckgs = [
+        ["torch-scatter", "-f", f"https://pytorch-geometric.com/whl/torch-{torch.__version__}+${cuda}.html"],
+        ["torch-sparse", "-f", f"https://pytorch-geometric.com/whl/torch-{torch.__version__}+${cuda}.html"],
+        ["torch-cluster", "-f", f"https://pytorch-geometric.com/whl/torch-{torch.__version__}+${cuda}.html"],
+        ["torch-spline-conv", "-f", f"https://pytorch-geometric.com/whl/torch-{torch.__version__}+${cuda}.html"],
+        [f"torch-geometric"]
+    ]
+
+    for p in pckgs:
+        subprocess.check_call([sys.executable, "-m", "pip", "install"] + p)
+
