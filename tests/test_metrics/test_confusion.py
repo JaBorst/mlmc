42cec5388a5e312bc49278c4773eb3c5c19d9abf
fz31ovo <fz31dovo@studserv.uni-leipzig.de>
2021-01-07 15:14:04 +0100

    Confusion matrix format test

--- tests/test_metrics/test_confusion.py
+++ tests/test_metrics/test_confusion.py
@@ -1,4 +1,5 @@
 from mlmc.metrics import ConfusionMatrix
+import pytest
 import torch
 
 def test_confusionMatrixSingle():
@@ -9,6 +10,9 @@ def test_confusionMatrixSingle():
     y_truth2 = torch.tensor([[0], [1], [2]])
     y_pred2 = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     example2 = (y_pred2, y_truth2)
+    y_pred3 = torch.tensor([[1, 1], [0, 1], [0, 1], [1, 0], [0, 1], [1, 1]])
+    y_truth3 = torch.tensor([[0, 0], [0, 1], [1, 1], [0, 1], [0, 1], [1, 1]])
+    example3 = (y_pred3, y_truth3)
 
     d.update(example1)
     assert torch.equal(torch.from_numpy(d.compute()), torch.tensor([[0, 1], [1, 0]]))
@@ -16,6 +20,11 @@ def test_confusionMatrixSingle():
     d.reset()
     d.update(example2)
     assert torch.equal(torch.from_numpy(d.compute()), torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+
+    d.reset()
+    d.update(example3)
+    with pytest.raises(ValueError):
+        d.compute()
 
 def test_confusionMatrixMulti():
     d = ConfusionMatrix(is_multilabel=True)
@@ -25,6 +34,9 @@ def test_confusionMatrixMulti():
     y_pred2 = torch.tensor([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])
     y_truth2 = torch.tensor([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])
     example2 = (y_pred2, y_truth2)
+    y_truth3 = torch.tensor([[0], [1]])
+    y_pred3 = torch.tensor([[0, 1], [1, 0]])
+    example3 = (y_pred3, y_truth3)
 
     d.update(example1)
     assert torch.equal(torch.from_numpy(d.compute()), torch.tensor([[[2, 2], [1, 1]], [[0, 1], [1, 4]]]))
@@ -32,3 +44,8 @@ def test_confusionMatrixMulti():
     d.reset()
     d.update(example2)
     assert torch.equal(torch.from_numpy(d.compute()), torch.tensor([[[4, 0], [0, 0]], [[4, 0], [0, 0]], [[4, 0], [0, 0]]]))
+
+    d.reset()
+    d.update(example3)
+    with pytest.raises(ValueError):
+        d.compute()
