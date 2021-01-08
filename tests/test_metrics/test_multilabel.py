8f20e4fc68e4cb8e2cb9af2ba4f835791b8d789f
fz31dovo <fz31dovo@studserv.uni-leipzig.de>
2021-01-08 08:11:15 +0100

    MultilabelReport format test

--- tests/test_metrics/test_multilabel.py
+++ tests/test_metrics/test_multilabel.py
@@ -1,5 +1,6 @@
 from mlmc.metrics import multilabel
 import torch
+import pytest
 
 def test_AUC():
     d = multilabel.AUC_ROC()
@@ -24,20 +25,26 @@ def test_AUC():
 def test_MultiLabelReport():
     classes1 = {"label_%i" % (i,): i for i in range(2)}
     classes2 = {"label_%i" % (i,): i for i in range(3)}
-    d1, d2, d3 = multilabel.MultiLabelReport(), multilabel.MultiLabelReport(), multilabel.MultiLabelReport()
-    d1.init(classes1, None)
-    d2.init(classes1, None)
-    d3.init(classes2, None)
+    d1, d2 = multilabel.MultiLabelReport(), multilabel.MultiLabelReport(is_multilabel = False)
 
-    y_pred = torch.tensor([[1, 1], [0, 1], [0, 1], [1, 0], [0, 1], [1, 1]])
-    y_truth = torch.tensor([[1, 1], [0, 1], [0, 1], [1, 0], [0, 1], [1, 1]])
-    example1 = (classes1, y_truth, y_pred)
-    y_pred = torch.tensor([[1, 1], [1, 1], [1, 0], [0, 1], [1, 1]])
-    y_truth = torch.tensor([[0, 1], [1, 0], [1, 1], [1, 1], [1, 1]])
-    example2 = (classes1, y_truth, y_pred)
-    y_pred = torch.tensor([[1, 0, 0], [0, 1, 0], [1, 0, 0], [0, 0, 1]])
-    y_truth = torch.tensor([[0, 1, 0], [1, 0, 0], [1, 0, 0], [0, 0, 1]])
-    example3 = (classes2, y_truth, y_pred)
+    #Multi-label examples
+    y_pred1 = torch.tensor([[1, 1], [0, 1], [0, 1], [1, 0], [0, 1], [1, 1]])
+    y_truth1 = torch.tensor([[1, 1], [0, 1], [0, 1], [1, 0], [0, 1], [1, 1]])
+    example1 = (classes1, y_truth1, y_pred1)
+    y_pred2 = torch.tensor([[1, 1], [1, 1], [1, 0], [0, 1], [1, 1]])
+    y_truth2 = torch.tensor([[0, 1], [1, 0], [1, 1], [1, 1], [1, 1]])
+    example2 = (classes1, y_truth2, y_pred2)
+    y_pred3 = torch.tensor([[1, 0, 0], [0, 1, 0], [1, 0, 0], [0, 0, 1]])
+    y_truth3 = torch.tensor([[0, 1, 0], [1, 0, 0], [1, 0, 0], [0, 0, 1]])
+    example3 = (classes2, y_truth3, y_pred3)
+
+    #Single-label examples
+    y_truth4 = torch.tensor([1, 0, 2])
+    y_pred4 = torch.tensor([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    example4 = (classes2, y_truth4, y_pred4)
+    y_truth5 = torch.tensor([0, 1, 1, 0])
+    y_pred5 = torch.tensor([[0, 1], [0, 1], [1, 0], [1, 0]])
+    example5 = (classes1, y_truth5, y_pred5)
 
     results1 = {'micro avg': {
                     'precision': 1.0,
@@ -50,6 +57,7 @@ def test_MultiLabelReport():
                     'f1-score': 1.0,
                     'support': 8}}
 
+    d1.init(classes1, None)
     d1.update(example1)
     assert d1.print() == results1
 
@@ -64,8 +72,10 @@ def test_MultiLabelReport():
                     'f1-score': 0.75,
                     'support': 8}}
 
-    d2.update(example2)
-    assert d2.print() == results2
+    d1.reset()
+    d1.init(classes1, None)
+    d1.update(example2)
+    assert d1.print() == results2
 
     results3 = {'micro avg': {
                     'precision': 0.5,
@@ -78,5 +88,37 @@ def test_MultiLabelReport():
                     'f1-score': 0.5,
                     'support': 4}}
 
-    d3.update(example3)
-    assert d3.print() == results3
+    d1.reset()
+    d1.init(classes2, None)
+    d1.update(example3)
+    assert d1.print() == results3
+
+    d1.reset()
+    d1.update(example4)
+    with pytest.raises(ValueError):
+        d1.print()
+
+    results4 = {'micro avg': {
+                    'precision': 0.3333333333333333,
+                    'recall': 0.3333333333333333,
+                    'f1-score': 0.3333333333333333,
+                    'support': 3},
+                'macro avg': {
+                    'precision': 0.3333333333333333,
+                    'recall': 0.3333333333333333,
+                    'f1-score': 0.3333333333333333,
+                    'support': 3}}
+
+    d2.init(classes2, None)
+    d2.update(example4)
+    assert d2.print() == results4
+
+    d2.reset()
+    d2.init(classes1, None)
+    d2.update(example5)
+    assert d2.print() == results3
+
+    d2.reset()
+    d2.update(example1)
+    with pytest.raises(ValueError):
+        d2.print()
