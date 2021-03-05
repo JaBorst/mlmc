b58762269dfe439dc71eb2ba4d1e7d5ecbb1338b
Janos Borst <borst@ifnormatik.uni-leipzig.de>
2021-03-05 18:12:35 +0100

    tempdir if path is none

--- mlmc/callbacks/cb_save.py
+++ mlmc/callbacks/cb_save.py
@@ -1,17 +1,27 @@
 from .abstract import Callback
 import pathlib
 import torch
+import shutil
+import tempfile
 
 class CallbackSaveAndRestore(Callback):
-    def __init__(self, path, file="model", metric="valid_loss", mode="min"):
+    def __init__(self, path=None, file="model", metric="valid_loss", mode="min", delete_after=True):
         super(CallbackSaveAndRestore, self).__init__()
         self.name = "callback_saveandrestore"
-        self.dir = pathlib.Path(path)
-        if not self.dir.exists(): self.dir.mkdir(parents=True)
+        if path is None:
+            self._using_tempdir=True
+            self.tmpdir = tempfile.TemporaryDirectory()
+            self.dir = pathlib.Path(str(self.tmpdir.name))
+            if not delete_after: Warning("If your not specifying a path the temporary directory will always be removed after training. `delete_after` has no effect.")
+        else:
+            self._using_tempdir=False
+            self.dir = pathlib.Path(path)
+            if not self.dir.exists(): self.dir.mkdir(parents=True)
         self.file = file
         self.metric = metric.split(".")
         self.mode = mode
         self.training_procedure = []
+        self.delete_after = delete_after
 
     def _add_metric(self, m):
         tmp = m.validation[-1]
@@ -27,3 +37,8 @@ class CallbackSaveAndRestore(Callback):
         i = torch.argmax(torch.tensor(self.training_procedure)) if self.mode=="max" else torch.argmin(torch.tensor(self.training_procedure))
         print(f"Restoring epoch {i+1}")
         model.load_state_dict(torch.load(self.dir / f"{self.file}_{i}.pt"))
+
+        if self._using_tempdir:
+            self.tmpdir.cleanup()
+        elif self.delete_after:
+            shutil.rmtree(self.dir)
