9cf40bf3c4a0a16de4a1eaea530c8c0747a1495f
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2021-06-18 09:25:55 +0200

    updated example for zeroshot models

--- examples/5-zeroshot-model.py
+++ examples/5-zeroshot-model.py
@@ -32,9 +32,9 @@ m = mlmc.models.zeroshot.EmbeddingBasedWeighted(
 # m = mlmc.models.zeroshot.EmbeddingBasedWeighted(
 #     classes={},
 #     target="single",
-#     mode="vanilla",
-#     sformatter=formatter,
+#     mode="max",
 #     finetune=True,
+#     sformatter=formatter,
 #     optimizer_params={"lr": 1e-5},
 #     device="cuda:3",  # If you have a GPU uncomment this
 #     representation="google/bert_uncased_L-12_H-768_A-12"
@@ -42,7 +42,7 @@ m = mlmc.models.zeroshot.EmbeddingBasedWeighted(
 # )
 
 # Zeroshot models have a method to switch and create new target classes
-classes1 = {"Sports":0, "Politics":1, "Science and Tech":2}
+classes1 = {"Sports":0, "Science":1}
 classes2 = {"Vacation": 0, "Work":1}
 
 m.create_labels(classes1)
@@ -51,30 +51,42 @@ print(m.predict("Did you watch the football match last night?"))
 print(m.predict("The laws of physics are complex."))
 
 m.create_labels(classes2)
-m.predict("I think I'll take a trip to Italy this week.")
-m.predict("I'm gonna have to stay late to finish this project.")
+print(m.predict("I think I'll take a trip to Italy this week."))
+print(m.predict("I'm gonna have to stay late to finish this project."))
 
 # These models can still be trained to match a specific set of labels
 # For example download agnews data. A data set containing news texts assigned to 4 categories
 data = mlmc.data.get("agnews")
 
-# We can switch to the label set of the data and evaluate. The model should have arount 49% accuracy without
-# seeing one training example:
+####
+# The larger model benefits from the formatter options. A formatter translates a label name into a short sentence.
+
+# We can switch to the label set of the data and evaluate. The smaller model achieve around 40 % accuracy, the
+# larger model should land around 36% accuracy without seeing any training example and without formatter:
+m.set_sformatter(lambda x: x)
 m.create_labels(data["classes"])
 print("Accuracy without training:", m.evaluate(data["test"])[1]["accuracy"])
 
+# Texts can formatter really do make a difference. Using the provided formatter for agnews,
+# this should increase to  51% for the smaller model and about 60 % for the larger model.
+m.set_sformatter(mlmc.data.SFORMATTER["agnews"])
+m.create_labels(data["classes"])
+m = m.eval()
+print("Accuracy without training with formatter:", m.evaluate(data["test"])[1]["accuracy"])
+
+
 
 # This Performance can still be improved of course by showing some of the training data.
-# We'll only take around 10 exapmles per class!
+# We'll only take around 10 exapmles per class.
 train = mlmc.data.sampler(data["train"], absolute=40)
 print(train.count())
 
 
 # Use the fit method to train the model
 m.loss = mlmc.loss.RelativeRankingLoss(0.5)
-m.fit(train, epochs=100)    # RelativeRankingLoss might go to zero. This is not bad thing but you can interrupt
+m.fit(train, epochs=50)    # RelativeRankingLoss might go to zero. This is not bad thing but you can interrupt
                             # the training at this point or set the number of epochs accodingly
 
 
-# For the large model this should achieve around 85 % accuracy, for the smaller instantiation around 76% accuracy.
+# For the large model this should achieve around 84 % accuracy, for the smaller instantiation around 70% accuracy.
 print("Accuracy after seeing few examples:", m.evaluate(data["test"])[1]["accuracy"])
