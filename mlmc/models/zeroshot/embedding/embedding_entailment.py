6dd0be6b85bd52fb37c440c5c91e0fdebb2d03ae
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2021-07-07 12:49:02 +0200

    experimental code

--- mlmc/models/zeroshot/embedding/embedding_entailment.py
+++ mlmc/models/zeroshot/embedding/embedding_entailment.py
@@ -6,7 +6,7 @@ class EmbeddingBasedEntailment(SentenceTextClassificationAbstract,TextClassifica
     """
      Zeroshot model based on cosine distance of embedding vectors.
     """
-    def __init__(self, mode="vanilla", entailment_output=3, *args, **kwargs):
+    def __init__(self, mode="vanilla",  *args, **kwargs):
         """
          Zeroshot model based on cosine distance of embedding vectors.
         This changes the default activation to identity function (lambda x:x)
@@ -24,8 +24,12 @@ class EmbeddingBasedEntailment(SentenceTextClassificationAbstract,TextClassifica
         self.set_mode(mode=mode)
 
         self.create_labels(self.classes)
-        self.parameter = torch.nn.Linear(self.embeddings_dim,256)
-        self.entailment_projection = torch.nn.Linear(3 * self.embeddings_dim, entailment_output)
+        self.bottle_neck = 512
+        self.parameter = torch.nn.Linear(self.embeddings_dim,self.bottle_neck)
+        self.parameter2 = torch.nn.Linear(self.bottle_neck + self.embeddings_dim,self.embeddings_dim)
+        self.att = torch.nn.TransformerEncoder(torch.nn.TransformerEncoderLayer(d_model=self.bottle_neck+self.embeddings_dim, nhead=8),num_layers=4)
+        self.entailment_projection = torch.nn.Linear(4*self.embeddings_dim, self.bottle_neck)
+        self.entailment_projection2 = torch.nn.Linear(self.bottle_neck, 3)
         self.build()
 
 
@@ -38,53 +42,59 @@ class EmbeddingBasedEntailment(SentenceTextClassificationAbstract,TextClassifica
         input_embedding = self.embedding(**x)[0]
         label_embedding = self.embedding(**self.label_dict)[0]
 
-        if "mean" in self.mode:
-            label_embedding = label_embedding - label_embedding.mean(0)
-            # print("mean added")
-
-
-        if "attention" in self.mode or "max" in self.mode:
-            input_embedding2 = input_embedding / input_embedding.norm(p=2, dim=-1, keepdim=True)
-            label_embedding2 = label_embedding / label_embedding.norm(p=2, dim=-1, keepdim=True)
-            word_scores = torch.einsum("ijk,lnk->iljn", input_embedding2, label_embedding2)
-            # print("attnetion or max added")
-
-        if "attention" in self.mode:
-            attentions = torch.relu(word_scores.mean(-1))
-            input_embedding = self._mean_pooling((attentions[..., None] * input_embedding[:, None]).transpose(1, 2),
-                                                 x["attention_mask"][:, :, None])
-            label_embedding = self._mean_pooling(label_embedding, self.label_dict["attention_mask"])
-            input_embedding = input_embedding / (input_embedding.norm(p=2, dim=-1, keepdim=True) + 1e-25)
-            label_embedding = label_embedding / (label_embedding.norm(p=2, dim=-1, keepdim=True) + 1e-25)
-        else:
-            input_embedding = self._mean_pooling(input_embedding, x["attention_mask"])
-            label_embedding = self._mean_pooling(label_embedding, self.label_dict["attention_mask"])
-            input_embedding = input_embedding / (input_embedding.norm(p=2, dim=-1, keepdim=True) + 1e-25)
-            label_embedding = label_embedding / (label_embedding.norm(p=2, dim=-1, keepdim=True) + 1e-25)
-
-
-
-        if self._config["target"]=="entailment":
-            r = self.entailment_projection(
-                torch.cat([input_embedding, label_embedding, torch.abs(input_embedding - label_embedding)],
-                          -1)).squeeze(-1)
-        else:
-            r = self.entailment_projection(
-                torch.cat([input_embedding[:,None].repeat(1,label_embedding.shape[0],1),
-                           label_embedding[None].repeat(input_embedding.shape[0],1,1),
-                           torch.abs(input_embedding[:,None] - label_embedding[None])],
-                          -1)).squeeze(-1)
 
         if self._config["target"] == "entailment":
-            return r
-        elif self._config["target"] == "single":
-            return torch.log(r[..., -1].softmax(-1))
-        elif self._config["target"] == "multi":
-            return torch.log(r[..., [0, 2]].softmax(-1)[:, -1])
-        elif self._config["target"] == "sts":
-            return torch.log(r[..., [0, 2]].softmax(-1)[:, -1])
+            le = self.parameter(label_embedding)
+            word_scores = torch.softmax(torch.einsum("ijk,ink->ijn", input_embedding, label_embedding),-1)
+            interaction = torch.einsum("bmn,bne->bme", word_scores, le)
+            interaction = torch.cat([input_embedding,interaction],-1)
+            interaction = self.att(interaction)
+            # word_scores = torch.softmax(torch.einsum("ijk,ink->ijn", interaction, le),-1)
+            # interaction = torch.einsum("bmn,bne->bme", word_scores, le)
+            interaction = self._mean_pooling(interaction, x["attention_mask"])
+            interaction = self.parameter2(interaction)
+
+
+            input_embedding = self._mean_pooling(input_embedding, x["attention_mask"])
+            label_embedding = self._mean_pooling(label_embedding, self.label_dict["attention_mask"])
+            e = torch.cat([input_embedding, label_embedding, torch.abs(input_embedding - label_embedding),interaction],-1)
+            # e = interaction
         else:
-            raise Exception
+            le = self.parameter(label_embedding)
+            word_scores = torch.softmax(torch.einsum("ijk,lnk->iljn", input_embedding, label_embedding),-1)
+            interaction = torch.einsum("blmn,lne->blme", word_scores, le)
+            interaction = torch.cat([input_embedding[:,None].repeat(1,interaction.shape[1],1,1),interaction],-1)
+            b,l,m,e = interaction.shape
+            interaction = self.att(interaction.reshape((b*l,m,e))).reshape((b,l,m,e))
+            # word_scores = torch.softmax(torch.einsum("ijk,lnk->iljn", interaction, le), -1)
+            # interaction = torch.einsum("blmn,lne->blme", word_scores, le)
+            interaction = self.parameter2(interaction)
+
+
+            input_mask_expanded = x["attention_mask"].unsqueeze(1).unsqueeze(-1).float()
+            sum_mask = torch.clamp(input_mask_expanded.sum(2), min=1e-9)
+            sum_embeddings = torch.sum(interaction * input_mask_expanded, 2)/sum_mask
+            input_embedding = self._mean_pooling(input_embedding, x["attention_mask"])
+            label_embedding = self._mean_pooling(label_embedding, self.label_dict["attention_mask"])
+
+            e = torch.cat([input_embedding[:,None].repeat((1,label_embedding.shape[0],1)),
+                           label_embedding[None].repeat((input_embedding.shape[0],1,1)),
+                           torch.abs(input_embedding [:,None]- label_embedding[None]),
+                           sum_embeddings],-1)
+            #
+            # e = sum_embeddings
+
+        logits = self.entailment_projection2(torch.tanh(self.entailment_projection(e)))
+
+        if self._config["target"] == "entailment":
+            pass
+        elif self._config["target"] == "single":
+            logits = torch.log(logits[...,-1].softmax(-1))
+        elif self._config["target"] == "multi":
+            logits = torch.log(e[..., [0, 2]].softmax(-1)[..., -1])
+        else:
+            assert not self._config["target"], f"Target {self._config['target']} not defined"
+        return logits
 
     def scores(self, x):
         """
@@ -121,7 +131,6 @@ class EmbeddingBasedEntailment(SentenceTextClassificationAbstract,TextClassifica
         else:
             self.set_loss(torch.nn.CrossEntropyLoss())
         self._all_compare=True
-        self.build()
 
     def multi(self, loss="ranking"):
         """Helper function to set the model into multi label mode"""
@@ -135,7 +144,6 @@ class EmbeddingBasedEntailment(SentenceTextClassificationAbstract,TextClassifica
         else:
             self.set_loss(torch.nn.BCELoss)
         self._all_compare=True
-        self.build()
 
     def sts(self):
         """Helper function to set the model into multi label mode"""
@@ -145,7 +153,6 @@ class EmbeddingBasedEntailment(SentenceTextClassificationAbstract,TextClassifica
         self.set_threshold("hard")
         self.set_activation(lambda x: x)
         self.set_loss(RelativeRankingLoss(0.5))
-        self.build()
 
     def entailment(self):
         self._config["target"] = "entailment"
@@ -155,4 +162,3 @@ class EmbeddingBasedEntailment(SentenceTextClassificationAbstract,TextClassifica
         self.set_activation(torch.softmax)
         self.set_loss = torch.nn.CrossEntropyLoss()
         self._all_compare = False
-        self.build()
