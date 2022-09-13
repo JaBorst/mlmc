3a07ed2b48a72af38a3f1e43c0867695b14efc70
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2022-09-13 14:12:51 +0200

    Experimental model

--- mlmc/models/zeroshot/generative.py
+++ mlmc/models/zeroshot/generative.py
@@ -7,10 +7,12 @@ class Generator(TextClassificationAbstract):
         self.build()
 
     def forward(self, x):
-        p = self.embedding(**x).logits.mean(1)#[:,-1,:]
-        # r = ((p[:,self.labels.to(self.device)["input_ids"]]*self.labels["attention_mask"][None]) / self.labels["attention_mask"][None].sum(-1, keepdims=True)).sum(-1)
-        r = ((p[:, self.labels.to(self.device)["input_ids"]] * self.labels["attention_mask"][None])).sum(-1)
-        return r
+        p = (self.embedding(**x).logits[:,-1,:]/ x["attention_mask"].sum(-1, keepdims=True)).log_softmax(-1)
+        # p = self.embedding(**x).logits[:,-self.labels["input_ids"].shape[-1]:,:]
+        r = ((p[:,self.labels["input_ids"]]*self.labels["attention_mask"][None]) / self.labels["attention_mask"][None].sum(-1, keepdims=True))
+        # i = torch.nn.functional.one_hot( self.labels["input_ids"].long(), num_classes=self.tokenizer.vocab_size)
+        # probs = (p[:,None] * i [None]).sum(-1) *  self.labels["attention_mask"][None] /  self.labels["attention_mask"][None].sum(-1, keepdims=True)
+        return r.sum(-1)
 
 
     def transform(self, x, h=None, max_length=None) -> dict:
@@ -33,9 +35,14 @@ class Generator(TextClassificationAbstract):
                                            "embedder, tokenizer = mlmc.representation.get_transformer()"
         if max_length is None:
             max_length = self._config["max_len"]
-        return {k: v.to(self.device) for k, v in
-                self.tokenizer(x, padding=True, max_length=max_length, truncation=True,
-                               add_special_tokens=True, return_tensors='pt', pad_to_max_length = True).items()}
+        h = ", ".join(self.classes.keys())
+        tok = self.tokenizer(x,[h] * len(x),  padding=True, max_length=max_length, truncation=True,
+                               add_special_tokens=True, return_tensors='pt', pad_to_max_length = True)
+        # new_shape = tuple(tok["input_ids"].shape)[:-1] + (self.labels["input_ids"].shape[-1],)
+        # tok["input_ids"] = torch.cat([tok["input_ids"], torch.full(new_shape, self.tokenizer.pad_token_id)],-1)
+        # tok["attention_mask"] = torch.cat([tok["attention_mask"], torch.full(new_shape, 1)],-1)
+        # if "token_type_ids" in tok.keys() : tok["token_type_ids"] = torch.cat([tok["token_type_ids"], torch.full(new_shape, 0)],-1)
+        return {k: v.to(self.device) for k, v in tok.items()}
 
 
     def create_labels(self, classes):
@@ -43,13 +50,13 @@ class Generator(TextClassificationAbstract):
         self._config["classes"] = classes
         self._config["n_classes"] = len(classes)
         self.n_classes = len(classes)
-        self.labels = self.tokenizer(list(self.classes.keys()), add_special_tokens=False, padding=True,
-                                     return_tensors="pt")
+        self.labels = {k:v.to(self.device) for k,v in self.tokenizer(list(self.classes.keys()), add_special_tokens=False, padding=True,
+                                     return_tensors="pt").items()}
 
     def _init_input_representations(self):
         from transformers import AutoModelForCausalLM, AutoTokenizer
         self.embedding = AutoModelForCausalLM.from_pretrained(self.representation, num_labels=3)
-        self.tokenizer = AutoTokenizer.from_pretrained(self.representation, padding_side="left", pad_token='¥')
+        self.tokenizer = AutoTokenizer.from_pretrained(self.representation, padding_side="left")
         # if self.tokenizer.pad_token is None: self.tokenizer.pad_token = '[PAD]'
         with torch.no_grad(): self.embeddings_dim = self.embedding(**self.tokenizer(["test"], return_tensors="pt"))[0].shape[-1]
 
