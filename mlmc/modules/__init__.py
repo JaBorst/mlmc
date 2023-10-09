11d868fda7da02f539a37fb9e61ce2966fbaf0d2
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2023-10-09 15:45:38 +0200

    removing graph modules

--- mlmc/modules/__init__.py
+++ mlmc/modules/__init__.py
@@ -1,4 +1,4 @@
 from mlmc.modules.layer_nc_attention_comparison import *
 from .module_KimCNN import KimCNNModule
-from .module_lsannc import LSANNCModule, LSANGraphModule, LSANVocabModule
+from .module_lsannc import LSANNCModule
 from .augment import  Augment
