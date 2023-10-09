f5a6da386ec05cd68fdd7dc86fbc9312d8857820
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2023-10-09 15:33:56 +0200

    remove global imports

--- mlmc/__init__.py
+++ mlmc/__init__.py
@@ -24,28 +24,4 @@ from .save_and_load import save, load
 import logging
 logging.getLogger("transformers.tokenization_utils_base").setLevel(logging.ERROR)
 
-import subprocess
-import sys
-import torch
-
-
-def install_torch_geometric(cuda=None):
-    if cuda is None:
-        cuda = torch.version.cuda
-    if cuda is None:
-        cuda = "cpu"
-    else:
-        cuda = f"cu{cuda.replace('.','')}"
-
-    version = torch.__version__.replace("+cpu", "")
-    pckgs = [
-        ["torch-scatter", "-f", f"https://pytorch-geometric.com/whl/torch-{version}+{cuda}.html"],
-        ["torch-sparse", "-f", f"https://pytorch-geometric.com/whl/torch-{version}+{cuda}.html"],
-        ["torch-cluster", "-f", f"https://pytorch-geometric.com/whl/torch-{version}+{cuda}.html"],
-        ["torch-spline-conv", "-f", f"https://pytorch-geometric.com/whl/torch-{version}+{cuda}.html"],
-        [f"torch-geometric"]
-    ]
-
-    for p in pckgs:
-        subprocess.check_call([sys.executable, "-m", "pip", "install"] + p)
 
--- mlmc/modules/__init__.py
+++ mlmc/modules/__init__.py
@@ -3,9 +3,3 @@ from .module_KimCNN import KimCNNModule
 from .module_lsannc import LSANNCModule, LSANGraphModule, LSANVocabModule
 from .dropout import VerticalDropout
 from .augment import  Augment
-try:
-    from mlmc_lab.mlmc_experimental.models.module_SKGML import SKGModule
-    from mlmc_lab.mlmc_experimental.models.module_ZAGCNN import ZAGCNNModule
-except:
-    print("torch_geometric not installed")
-
