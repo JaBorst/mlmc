ea1679c606d5e670ce1778a3ea3f2443783b6d10
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2023-10-09 15:36:57 +0200

    clean some modules

--- mlmc/modules/__init__.py
+++ mlmc/modules/__init__.py
@@ -1,5 +1,4 @@
 from mlmc.modules.layer_nc_attention_comparison import *
 from .module_KimCNN import KimCNNModule
 from .module_lsannc import LSANNCModule, LSANGraphModule, LSANVocabModule
-from .dropout import VerticalDropout
 from .augment import  Augment
